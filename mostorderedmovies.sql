cd4d2cf011222ef619daa0a02448ec92dfa22868
navpreetkaur95 <40548844+navpreetkaur95@users.noreply.github.com>
2018-09-10 14:46:27 +1200

    Add files via upload

---
+++ mostorderedmovies.sql
@@ -0,0 +1,2 @@
+﻿Select MovieId,Title,count(rmid) as ordertimes from Movies, rentedmovies 
+where Movies.MovieID=RentedMovies.MovieIDFK group by MovieId,Title order by ordertimes desc
---
+++ mostorderingcustomers.sql
@@ -0,0 +1,2 @@
+﻿select FirstName,LastName,count(rmid) as ordertimes from customer, rentedmovies 
+where Customer.CustID=RentedMovies.CustIDFK group by FirstName,LastName order by ordertimes desc
